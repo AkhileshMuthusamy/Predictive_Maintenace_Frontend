bc7d79958d0ebe895fbd833776f684762eda8469
Akhilesh Muthusamy <akhileshm@outlook.in>
2021-12-10 20:16:19 -0500

    Updated Production URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'http://ec2-18-222-161-93.us-east-2.compute.amazonaws.com:8080/',
+  apiURL: 'https://ec2-18-216-60-243.us-east-2.compute.amazonaws.com:8080/',
 };
