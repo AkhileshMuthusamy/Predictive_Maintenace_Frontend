6346e52513cd4e5815e66710b5c6ebd96609a38f
Akhilesh Muthusamy <akhileshm@outlook.in>
2021-12-11 00:10:45 -0500

    Pointing to Heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiURL: 'https://product-maintenance.herokuapp.com/',
-  apiURL: 'http://ec2-18-216-60-243.us-east-2.compute.amazonaws.com:8080/',
+  apiURL: 'https://product-maintenance.herokuapp.com/',
+  // apiURL: 'http://ec2-18-216-60-243.us-east-2.compute.amazonaws.com:8080/',
 };
